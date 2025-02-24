227c9d08bba0104daca87519e419aa745d4eaace
pablichoka <pablikoruab98@gmail.com>
2025-02-24 21:12:16 +0100

    Add repositories

---
+++ src/main/java/com/pruebabackonebox/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.pruebabackonebox.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pruebabackonebox.model.Product;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+  public Optional<Product> findByDescription(String description);
+
+  public List<Product> findAll();
+
+  public boolean existsById(Integer id);
+
+}
