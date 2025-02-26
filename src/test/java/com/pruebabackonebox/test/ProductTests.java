2a37a65450afb79e1653f548d32815fa1e31e682
pablichoka <pablikoruab98@gmail.com>
2025-02-26 21:43:24 +0100

    Minor fixes on product tests

--- src/test/java/com/pruebabackonebox/test/ProductTests.java
+++ src/test/java/com/pruebabackonebox/test/ProductTests.java
@@ -3,9 +3,12 @@ package com.pruebabackonebox.test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,33 +32,36 @@ class ProductTests {
 		this.objectMapper = objectMapper;
 	}
 
-	@Test
-	public void testAddProduct() throws Exception {
-		ProductDTO product = new ProductDTO("Producto 1", 2.0);
+	@BeforeEach
+	public void addInitialProduct() throws Exception {
+		ProductDTO product = new ProductDTO("Producto 1", 3.0);
 
 		mockMvc.perform(post("/product/add")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(product)))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.description").value("Producto 1"))
+				.andExpect(jsonPath("$.amount").value(3.0));
+	}
+
+	@Test
+	public void testAddProduct() throws Exception {
+		ProductDTO product = new ProductDTO("Producto 2", 2.0);
+
+		mockMvc.perform(post("/product/add")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(product)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.description").value("Producto 2"))
 				.andExpect(jsonPath("$.amount").value(2.0));
 	}
 
 	@Test
 	public void testGetProduct() throws Exception {
-		ProductDTO product = new ProductDTO("Producto 1", 42.0);
-
-		mockMvc.perform(post("/product/add")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(product)))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.description").value("Producto 1"))
-				.andExpect(jsonPath("$.amount").value(42.0));
-
 		mockMvc.perform(get("/product/{id}", 1))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.description").value("Producto 1"))
-				.andExpect(jsonPath("$.amount").value(42.0));
+				.andExpect(jsonPath("$.description").value("Producto 2"))
+				.andExpect(jsonPath("$.amount").value(2.0));
 	}
 
 	@Test
@@ -68,8 +74,20 @@ class ProductTests {
 	@Test
 	public void testDeleteProduct() throws Exception {
 		mockMvc.perform(delete("/product/delete")
-				.param("id", "1"))
+				.param("id", "2"))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testUpdateProduct() throws Exception {
+		ProductDTO product = new ProductDTO("Producto 1 Modificado", 4.0);
+
+		mockMvc.perform(put("/product/update/{id}", 1)				
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(product)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.description").value("Producto 1 Modificado"))
+				.andExpect(jsonPath("$.amount").value(4.0));
 	}
 
 }
